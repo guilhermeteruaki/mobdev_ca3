15d1c5d7384ffb38a56887afa5e16bf307668c25
Guilherme Teruaki <guilherme.teruaki@gmail.com>
2022-11-18 21:08:25 +0000

    character details working

--- src/app/pages/film-details/film-details.page.ts
+++ src/app/pages/film-details/film-details.page.ts
@@ -1,6 +1,3 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-film-details',
